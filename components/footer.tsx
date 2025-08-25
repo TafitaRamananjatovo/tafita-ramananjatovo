19b255ffdd9654fc0f394b7d4d79b38197b22fbb
TafitaRamananjatovo <tafita.ramananjatovo@gmail.com>
2025-08-25 05:48:44 +0300

    Changed on the frontend

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,11 +3,12 @@ import React from "react";
 export default function Footer() {
   // Obtenir l'année actuelle
   const currentYear = new Date().getFullYear();
+  const startYear = 2024;
   
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; {currentYear} Tafita. All rights reserved.
+        &copy; {startYear} - {currentYear} Tafita. All rights reserved.
       </small>
     </footer>
   );
